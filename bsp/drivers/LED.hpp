5b5dec942a90af4cbda9637d1a6ccce1eda34955
CaFeZn <1837781998@qq.com>
2025-09-16 15:40:04 +0800

    功能(LED)：新增LED_State和DriveMode枚举，优化API接口
    引入LED_State枚举，包含OFF、ON和TOGGLE状态，为LED操作提供清晰的状态管理。新增DriveMode枚举，支持ActiveHigh和ActiveLow两种驱动配置。更新LED构造函数以接受DriveMode参数，并增加read()方法以读取当前LED状态。这些改进增强了LED控制的灵活性，并提供了更好的状态跟踪能力。

--- bsp/drivers/LED.hpp
+++ bsp/drivers/LED.hpp
@@ -3,6 +3,7 @@
 
 #include "GPIO.hpp"
 
+
 /**
  * @brief LED 驱动类
  *
@@ -13,13 +14,35 @@
  */
 class LED : public GPIO {
 public:
-    using GPIO::GPIO;   // 继承构造函数
-    
-    void on(void);
+    enum LED_State {
+        OFF = 0,
+        ON,
+        TOGGLE,
+    };
+
+	enum DriveMode {
+		ActiveHigh, // 高电平点亮
+		ActiveLow	// 低电平点亮
+	};
+
+	LED(GPIO_TypeDef* port, uint16_t pin, DriveMode drive_mode);
+
+	void on(void);
 
     void off(void);
 
 	using GPIO::toggle; // 继承翻转函数 // 显式把 toggle 暴露在 LED 类
+    
+    LED_State read(void);
+
+    DriveMode drivemode(void){
+        return _drive_mode;
+    }
+
+private:
+    DriveMode _drive_mode;
+  
 };
+
 
 #endif //! __LED_HPP
