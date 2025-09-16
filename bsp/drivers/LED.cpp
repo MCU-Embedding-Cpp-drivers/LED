c43baba8ca2c54198f3953dac0388426bb84d305
CaFeZn <1837781998@qq.com>
2025-09-16 22:58:45 +0800

    补全了高低电平驱动的break语句 将LED构造函数适配GPIO构造函数

--- bsp/drivers/LED.cpp
+++ bsp/drivers/LED.cpp
@@ -1,6 +1,5 @@
 #include "LED.hpp"
 #include "GPIO.hpp"
-#include "stm32g4xx_hal_gpio.h"
 
 /**
  * @brief LED构造函数，构造一个LED对象
@@ -11,7 +10,7 @@
  * @arg LED::ActiveHigh 高电平点亮
  * @arg LED::ActiveLow  低电平点亮
  */
-LED::LED(GPIO_TypeDef* port, uint16_t pin, DriveMode drive_mode) : GPIO(port, pin), _drive_mode(_drive_mode) {
+LED::LED(void* port, uint16_t pin, DriveMode drive_mode) : GPIO(port, pin), _drive_mode(drive_mode) {
 }
 
 /**
@@ -22,9 +21,11 @@ void LED::on(void) {
 	switch (_drive_mode) {
 		case LED::ActiveHigh: // 高电平驱动
 			GPIO::write(1);
+			break;
 
 		case LED::ActiveLow: // 低电平驱动
 			GPIO::write(0);
+			break;
 
 		default: // 异常情况，_drive_mode被意外修改
 			return;
@@ -39,9 +40,11 @@ void LED::off(void) {
 	switch (_drive_mode) {
 		case LED::ActiveHigh: // 高电平驱动
 			GPIO::write(0);
+			break;
 
 		case LED::ActiveLow: // 低电平驱动
 			GPIO::write(1);
+			break;
 
 		default: // 异常情况，_drive_mode被意外修改
 			return;
@@ -56,10 +59,10 @@ void LED::off(void) {
 LED::LED_State LED::read(void) {
 	switch (_drive_mode) {
 		case LED::ActiveHigh:
-			return (GPIO::read() == GPIO_PIN_SET) ? ON : OFF;
+			return (GPIO::read() == 1) ? ON : OFF;
 
 		case LED::ActiveLow:
-			return (GPIO::read() == GPIO_PIN_RESET) ? OFF : ON;
+			return (GPIO::read() == 0) ? ON : OFF;
 
 		default: // 异常情况，_drive_mode被意外修改
 			return OFF;
--- bsp/drivers/LED.hpp
+++ bsp/drivers/LED.hpp
@@ -3,7 +3,6 @@
 
 #include "GPIO.hpp"
 
-
 /**
  * @brief LED 驱动类
  *
@@ -12,37 +11,36 @@
  * - off()      : 熄灭
  * - toggle()   : 翻转（继承自 GPIO）
  */
-class LED : public GPIO {
-public:
-    enum LED_State {
-        OFF = 0,
-        ON,
-        TOGGLE,
-    };
+class LED : public GPIO
+{
+  public:
+	enum LED_State {
+		OFF = 0,
+		ON,
+		TOGGLE,
+	};
 
 	enum DriveMode {
 		ActiveHigh, // 高电平点亮
 		ActiveLow	// 低电平点亮
 	};
 
-	LED(GPIO_TypeDef* port, uint16_t pin, DriveMode drive_mode);
+	LED(void* port, uint16_t pin, DriveMode drive_mode);
 
 	void on(void);
 
-    void off(void);
+	void off(void);
 
 	using GPIO::toggle; // 继承翻转函数 // 显式把 toggle 暴露在 LED 类
-    
-    LED_State read(void);
 
-    DriveMode drivemode(void){
-        return _drive_mode;
-    }
+	LED_State read(void);
 
-private:
-    DriveMode _drive_mode;
-  
+	DriveMode drivemode(void) {
+		return _drive_mode;
+	}
+
+  protected:
+	DriveMode _drive_mode;
 };
-
 
 #endif //! __LED_HPP
